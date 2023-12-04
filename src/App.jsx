5fdf6d93655b4931d0aa9e631e309cf8b79d72a0
Gustavo Fernandes <goncalves_017@hotmail.com>
2023-12-04 14:58:52 -0300

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LoginSignup } from './Components/LoginSignup/LoginSignup';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <LoginSignup />
+    </div>
+  );
+};
+
+export default App;
